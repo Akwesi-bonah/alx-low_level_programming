31e817b27aefa19fcdd2478983217ff655536e2b
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-17 10:02:37 -0700

    lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+/**
+ * main - Entry
+ * Description: print alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+
+	return (0);
+}
