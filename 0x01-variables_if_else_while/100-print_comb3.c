869a713f377d41ce4fccdc819be91334eba11b43
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-17 13:16:19 -0700

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,8 +15,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar((num % 10) + '0');
-			putchar((num % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 8 && num2 == 9)
 			{
