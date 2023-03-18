805a2a50fb9e40eda22dd68c86dbb3f40bd80ae6
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-18 15:22:09 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Description: printing numbers form 0 - 100
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num < 100; num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
