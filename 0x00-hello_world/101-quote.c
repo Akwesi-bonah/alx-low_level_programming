320906f2af9fa1875c9caad2b78c7e5bb46327db
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-18 15:50:21 -0700

    exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Description : print exactly "and the piece of art is usefull"
  *
- * Retrun: 0 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
