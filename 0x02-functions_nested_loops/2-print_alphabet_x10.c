c662f9326df874ee274c7be288a1e025021bb3b9
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-21 07:28:25 -0700

    lower case letter 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 10: num ++)
+	for (num = 0; num <= 10; num ++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
