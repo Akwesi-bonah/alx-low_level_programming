1d1e5cbf323790b79e1a39af5f03e20b8f6dd4ec
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-21 08:25:58 -0700

    absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ * _abs - check if a number is positive
+ *@n: number to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	return (-n);
+}
