3b9279d6db1e326fe5a4a6a8925f46857a3147e3
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-22 20:10:47 -0700

    sum of event numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints sum of even  number < 4,000,000
+ * followed by a new line
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int x = 0, y = 1, count = 0;
+	int sum = 0;
+
+	while (count < 4000000)
+	{
+		count = x + y;
+		x = b;
+		y = count;
+		if (next % 2 == 0)
+			sum += count;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
