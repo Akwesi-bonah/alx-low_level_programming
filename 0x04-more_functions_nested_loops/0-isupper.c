687811dd931c1daa12b7f8752c195adab359a741
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 10:06:38 -0700

    check if character is upper case letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - check if a char is uppre case letter
+ * @c: character to check
+ *
+ * Return: 1 if character is upper case letter else 0
+ */
+
+int _isupper(char c)
+{
+	return (c >= 65 && c <= 90);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAINH
+
+int putchar(char c);
+int _isupper(int c);
+
+#endif
