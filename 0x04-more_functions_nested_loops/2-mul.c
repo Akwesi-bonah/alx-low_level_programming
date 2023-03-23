af5493242b51cacfab5ff01090936b848d4dfbf7
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 10:47:44 -0700

    multiply to numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiple two integers
+ * @a: integer to perform the operation
+ * @b: int to perform the operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
