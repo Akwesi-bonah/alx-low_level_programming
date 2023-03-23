e8b433b9c516ad217f3896a3381fb00321259985
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 14:08:51 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+# include "main.h"
+/**
+ * print_square - print square of a number
+ * @size: limit
+ */
+
+void print_square(int size)
+{
+	int i, k;
+
+	if (n > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; j++)
+			{
+				_putchar('#');
+				_putchar('\n);
+			}
+		}
+	}
+	else
+	{
+		_puchar('\n');
+	}
+}
