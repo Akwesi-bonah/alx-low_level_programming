af8875df72587e9cd2da03f58bac9f2e04a70669
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 10:26:26 -0700

    check if a character is a number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+ * _isdigit - check if a character is between 0 - 9
+ *
+ * @c: character to check
+ * Return: 1 if  then character is  a number else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 && <= 9);
+}
