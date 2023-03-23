23cd54f0ce24d399906ea64f12f32cb054209214
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 14:34:37 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+/**
+ * main - Entery
+ * Description: print Fizz buzz
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
