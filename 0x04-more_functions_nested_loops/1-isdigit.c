9e18549fb2d490ad16be9d2f522ffc0405ce98a7
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 10:40:32 -0700

    fixed error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-# include "main.h"
+#include "main.h"
 /**
  * _isdigit - check if a character is between 0 - 9
  *
@@ -8,5 +8,5 @@
 
 int _isdigit(int c)
 {
-	return (c >= '0' && <= '9');
+	return (c >= '0' && c <= '9');
 }
