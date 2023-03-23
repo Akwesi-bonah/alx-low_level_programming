09fd40b3f3343892b61241ac9267502f82d17568
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 13:48:47 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+# include "main.h"
+/**
+ * print_line - print line
+ * @n: checker
+ * end with new line character
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
