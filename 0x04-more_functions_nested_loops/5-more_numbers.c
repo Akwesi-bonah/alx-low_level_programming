0c6bb8f0b05c72e4b5427019f36fc2f3478037b9
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-23 13:31:55 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 			{
@@ -19,6 +19,7 @@ void more_numbers(void)
 
 			_putchar(j % 10 + '0');
 		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
