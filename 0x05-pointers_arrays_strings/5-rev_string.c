c9c099e7cb679414aef5874cf016777c025b1101
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-27 09:47:57 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+# include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer
+ */
+
+void rev_string(char *s)
+{
+	char var;
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i - 1;
+
+	for (j = 0; j < len / 2; j++)
+	{
+		var = s[j];
+		s[j] = s[len];
+		s[len--] = var;
+	}
+}
