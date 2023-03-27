fcc82ec7b7b8f3e731796a1a9f4cb75e54ab63f7
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-27 09:33:43 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+# include "main.h"
+/**
+ * print_rev - print string in reverse 
+ * @s: pointer to print
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i - 1;
+
+	for(j = len; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
