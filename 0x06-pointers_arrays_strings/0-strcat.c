ab4f05cfd6f5045097c93f69953497bc74baf632
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-29 08:53:23 -0700

    add string together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+# include "main.h"
+/**
+ * _strcat - join two string together
+ * @src: source to append
+ * @dest: source to add 
+ *
+ * Retur: The result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] = '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
