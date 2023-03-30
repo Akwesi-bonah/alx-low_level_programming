a9c8dc250aa170a35fb264204eb7faeacab7e9a8
Akwesi-bonah <arhinbonnah@gmail.com>
2023-03-30 06:53:46 -0700

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *s);
+char *rot13(char *s);
 #endif
