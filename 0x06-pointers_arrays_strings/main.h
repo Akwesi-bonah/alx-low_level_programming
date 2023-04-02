6bf0995f74c81504c91c309f25f20c2cc6a05066
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-02 01:30:21 -0700

    solved

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
