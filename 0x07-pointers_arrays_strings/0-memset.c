cd38f578bd4a2297ce017101c68885a0bfb438fd
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-03 06:59:28 -0700

    fixed

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-       	int i;
+	unsigned int i;
 
-       	for (i = 0; i < n; i++)
-       	{
-       	       	s[i] = b;
-       	}
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-       	return (s);
+	return (s);
 }
