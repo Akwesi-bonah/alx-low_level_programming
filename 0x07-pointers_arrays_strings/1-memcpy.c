64d98a78e35e015154ab465e31436ad1f489c04c
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-03 07:07:25 -0700

    fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-       	unsigned int i;
+	int i = 0;
 
-       	for (i = 0; i < n; i++)
-       	{
-       	       	dest[i] = src[i];
-       	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-       	return (dest);
+	return (dest);
 }
 
