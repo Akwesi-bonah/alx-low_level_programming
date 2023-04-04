3340ef5ab10ee05cb97fb6f7bc3aed76ce75c38c
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-04 15:06:14 -0700

    copy pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+ * to_string - set pointer to char
+ * @s: pointer to store
+ * @to: string to change
+ * Return: 0
+ */
+
+void to_string( char **s, char *to)
+{
+	*s = to;
+}
