efeb0d8e29a6b59b6b30c019a9d07eaf08daa525
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-06 20:20:38 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,15 @@
 # include "main.h"
 
 /**
- * checker - _sqrt_recursion
+ * checker - check th number
+ * Description: sqaure root of number
  * @n: int
  * @a: int for checker
  *
  * Return: checker
  */
 
-int checker(int n, int a)
-{
-        if (n < 0)
-        {
-                return (-1);
-        }
-        if ((a * a) > n)
-        {
-                return (-1);
-        }
-        if (a * a == n)
-        {
-                return (a);
-        }
-        return (checker(n, a + 1));
-}
-
+int checker(int n, int a);
 
 /**
  * _sqrt_recursion - print square root of a number
