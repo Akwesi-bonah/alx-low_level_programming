8e99511c53d6b05bf1e02b663406f2e013888581
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-07 06:41:11 -0700

    programe name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * main - print the progam name
+ * @argc: int argument
+ * @argv: string pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+# Arguments to main
+
+The declaration of main looks like this:
+
+`int main(int argc, char *argv[]);`
+This indicates that main is a function returning an integer.
