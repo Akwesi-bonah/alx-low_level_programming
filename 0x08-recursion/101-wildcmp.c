500f71ca0703dbbd9cb8738e6d2a40a852a389f1
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-07 02:23:55 -0700

    corrections made

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,8 +13,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-
-	if (!*s1 && *s2 == '*' && *(s2 + 1) == '\0')
+	if (!*s1 && *s2 == '*' && *(s2 + 1) != '\0')
 	{
 		return (0);
 	}
@@ -26,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		return (wildcmp((s1), (s2 + 1) )|| wildcmp((s1 + 1), s2));
+		return (wildcmp((s1), (s2 + 1)) || wildcmp((s1 + 1), s2));
 	}
 
 	return (0);
