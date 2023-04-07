64af0c58e70912860f610ac19806112ff4d70814
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-07 07:39:58 -0700

    print all argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * main - print all argument passed
+ * @argc: int argument
+ * @argv: string pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i= 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
