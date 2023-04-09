77076d8f5d759b49d50efc75009918f3f7be2a28
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-09 04:35:28 -0700

    all files

---
+++ lib/0-strcat.c
@@ -0,0 +1,33 @@
+# include "main.h"
+/**
+ * _strcat - join two string together
+ * @src: source to append
+ * @dest: source to add
+ *
+ * Return: The result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
