949f322684ac11a2e16e7f8ef6212bc7f2f2a456
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-12 03:56:52 -0700

    checked

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,9 +12,8 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid, i, j;
-	int len = width * height;
 
-	if (len <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	grid = (int **)malloc(sizeof(int) * height);
@@ -24,20 +23,17 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			free(grid);
-			for (j = 0; j <= i; j++)
+			while (i >= 0)
 			{
-				free(grid[j]);
+				free(grid[i]);
+				i--;
 			}
 			return (NULL);
 		}
-	}
 
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
