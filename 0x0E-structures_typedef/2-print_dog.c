46e28d5784b9c1bbb2701e12cf0bee75db98c1df
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-17 13:56:15 -0700

    free dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 
 	if (d->age < 0)
 	{
-		printf("Age: nil\n");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free  dog memory
+ * @d: pointer to dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
