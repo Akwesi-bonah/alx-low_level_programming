017aa3c90e494d17854eee0403c9add4160d53b9
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-17 13:37:13 -0700

    new dod struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("nil\n");
+		printf("Name: nil\n");
 	}
 	else
 	{
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 
 	if (d->age < 0)
 	{
-		printf("nil\n");
+		printf("Age: nil\n");
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("nil\n");
+		printf("Owner: nil\n");
 	}
 	else
 	{
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - struct of new dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
