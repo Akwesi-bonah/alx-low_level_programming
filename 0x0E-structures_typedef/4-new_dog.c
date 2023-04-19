539a006f9e73877471bd1e21cdccafe242d6ebdc
Akwesi-bonah <85927459+Akwesi-bonah@users.noreply.github.com>
2023-04-19 06:50:47 +0000

    review

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy;
 
-	if (name == NULL || owner == NULL || age < 0)
+	if (name == NULL || owner == NULL )
 		return (NULL);
 
 	puppy = malloc(sizeof(dog_t));
