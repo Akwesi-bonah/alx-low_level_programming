f850270690adf15cdd80adb07e8a400056dffb94
Akwesi-bonah <arhinbonnah@gmail.com>
2023-05-05 17:55:34 -0700

    fixed

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,14 +1,27 @@
-#include "main.h"
+/**
+ * _bit - sets the value of a bit to 1 at a given index
+ * @n: Number
+ * @index: index, starting from 0
+ * Return: value of the bit at index else -1
+ */
+int _bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a = 1;
+
+	if (index > 64)
+		return (-1);
+	*n = a << index | *n;
+	return (1);
+}
 
 /**
- * clear_bit - set value of a bits to 0
- * @n: number to be converted
- * @index: stating from 0
- *
- * Return: index value of bit else -1
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number
+ * @index: index starting form 0
+ * Return: value of the bit at index else -1
  */
 
-int clear_bit(unsined long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int a = ULONG_MAX - 1;
 	unsigned int i;
@@ -23,29 +36,7 @@ int clear_bit(unsined long int *n, unsigned int index)
 		a = a << 1;
 		_bit(&a, 0);
 	}
+
 	*n = a & *n;
-
-	return (1);
-}
-
-/**
- * _bit - set a value of bit to 1 for a given index
- * @n: number to be converted
- * @index: starting index 0 of the bit
- *
- * Return: index value else -1
- */
-
-int _bit(unsigned long int *n, unsigned int index)
-{
-	unsigned long int val = 1;
-
-	if (index > 64)
-	{
-		return (-1);
-	}
-
-	*n = a << index | *n;
-
 	return (1);
 }
