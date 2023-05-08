8c860fd5b309083f3102da4d06b919db91405415
Akwesi-bonah <arhinbonnah@gmail.com>
2023-05-08 19:14:19 +0000

    intro

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints standard output
+ * @filename: file to print it content
+ * @letters: characters to be printed
+ *
+ * Return: 0 if filename is NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t reader; writer;
+	int count;
+	char *words;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	count = open(filename, O_RDONLY);
+	if (count == -1)
+	{
+		return (0);
+	}
+
+	words = malloc(letters);
+	if (words == NULL)
+	{
+		close(words);
+		return (0);
+	}
+
+	reader  = read(words, count, letters);
+	writer = write(STDOUT_FILENO, words, reader);
+
+	if (writer != reader)
+	{
+		close(count);
+		free(words);
+
+		return (0);
+	}
+	close(count);
+	free(words);
+
+	return (writer);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+ * create_file - e a function that appends text at the end of a file
+	 * 
